df32f25af682061e0ede4bd3fb1efbf74e02e720
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-02-04 07:34:41 -0700

    Countdown timer coerce

--- PSYCH_213/countdown.js
+++ PSYCH_213/countdown.js
@@ -5,6 +5,7 @@ const countdownData = [
   { label: "Achieve Countdown", date: "2025-03-04T15:30:00", timezone: "America/Edmonton", containerId: "Achieve_1" },
   { label: "Achieve Countdown", date: "2025-04-09T23:59:00", timezone: "America/Edmonton", containerId: "Achieve_2" },
   { label: "Homework 1 Countdown", date: "2025-02-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_1" },
+  { label: "Homework 1 Countdown", date: "2025-02-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_1_2" },
   { label: "Homework 2 Countdown", date: "2025-02-13T23:59:00", timezone: "America/Edmonton", containerId: "HW_2" }
 ];
 
