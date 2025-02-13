3aad6de118a9c0965e812222c4b032866ad48465
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-02-13 13:46:28 -0700

    Added HW_4

--- PSYCH_213/countdown.js
+++ PSYCH_213/countdown.js
@@ -4,11 +4,10 @@ const countdownData = [
   { label: "Final", date: "2025-04-22T13:00:00", timezone: "America/Edmonton", containerId: "Final" },
   { label: "Achieve Countdown", date: "2025-03-04T15:30:00", timezone: "America/Edmonton", containerId: "Achieve_1" },
   { label: "Achieve Countdown", date: "2025-04-09T23:59:00", timezone: "America/Edmonton", containerId: "Achieve_2" },
-  { label: "Homework 1 Countdown", date: "2025-02-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_1" },
   { label: "Homework 1 Countdown", date: "2025-02-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_1_2" },
-  { label: "Homework 2 Countdown", date: "2025-02-13T23:59:00", timezone: "America/Edmonton", containerId: "HW_2" },
   { label: "Homework 2 Countdown", date: "2025-02-13T23:59:00", timezone: "America/Edmonton", containerId: "HW_2_2" },
-  { label: "Homework 3 Countdown", date: "2025-02-20T23:59:00", timezone: "America/Edmonton", containerId: "HW_3" }
+  { label: "Homework 3 Countdown", date: "2025-02-20T23:59:00", timezone: "America/Edmonton", containerId: "HW_3" },
+  { label: "Homework 4 Countdown", date: "2025-02-27T23:59:00", timezone: "America/Edmonton", containerId: "HW_4" }
 ];
 
 // Function to create a timer in a specific container
