1a5546ff37230739492fb3f28019cda789356670
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-02-15 13:51:51 -0700

    Updated countdown

--- PSYCH_213/countdown.js
+++ PSYCH_213/countdown.js
@@ -6,7 +6,7 @@ const countdownData = [
   { label: "Achieve Countdown", date: "2025-04-09T23:59:00", timezone: "America/Edmonton", containerId: "Achieve_2" },
   { label: "Homework 1 Countdown", date: "2025-02-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_1_2" },
   { label: "Homework 2 Countdown", date: "2025-02-13T23:59:00", timezone: "America/Edmonton", containerId: "HW_2_2" },
-  { label: "Homework 3 Countdown", date: "2025-02-27T23:59:00", timezone: "America/Edmonton", containerId: "HW_3" },
+  { label: "Homework 3 Countdown", date: "2025-02-27T23:59:00", timezone: "America/Edmonton", containerId: "HW_3_2" },
   { label: "Course Project Countdown", date: "2025-03-07T23:59:00", timezone: "America/Edmonton", containerId: "CP_I" }
 ];
 
