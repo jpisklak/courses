f7b46f4d54a0b1588e3ef41b23fd39b3d6c02f29
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-05-02 15:27:01 -0600

    -u

--- PSYCH_413_sp2025/data_files/table_code/table_gen.R
+++ PSYCH_413_sp2025/data_files/table_code/table_gen.R
@@ -1,25 +1,33 @@
 library(tidyverse)
-library(xtable)
+library(knitr)
+library(kableExtra)
+library(clipr)
 options(pillar.print_min = 30)
 
-# Primary List of Data files
-#-------------------------------------------------------------------------------
+# Define directory and GitHub repo info
+local_dir <- ".."
+repo_url <- "https://github.com/jpisklak/courses/tree/main/PSYCH_413_sp2025"
+github_path <- "data_files"  # the relative path to the folder in the GitHub repo
 
-prime_list <- list.files("..", include.dirs = FALSE)
-prime_list_url <- paste0(
-  'https://github.com/jpisklak/courses/blob/main/PSYCH_413/data_files/', prime_list)
+# List all CSV files
+csv_files <- list.files(local_dir, pattern = "\\.csv$", full.names = FALSE)
 
-prime_list_html <- paste0(
-  '<a href = "', prime_list_url, '">', 
-  prime_list, '</a>')
+# Create GitHub URLs
+df <- data.frame(
+  Filename = csv_files,
+  GitHub_Link = paste0(repo_url, "/", github_path, "/", csv_files),
+  Raw_URL = paste0("https://raw.githubusercontent.com/jpisklak/courses/refs/heads/main/PSYCH_413_sp2025/", 
+                   github_path, "/", csv_files), stringsAsFactors = FALSE)
 
 
-df <- tibble(
-  `General Data Files` = prime_list_html
-)
+# Create an HTML table
+df$Filename <- paste0("<a href='", df$GitHub_Link, "' target = '_blank'>", df$Filename, "</a>")
 
-print(xtable(df, align = c("l", "l")), 
-      include.rownames = FALSE,
-      type="html", 
-      file="prime_table.html")
+# Render the table
+html_tab <- kable(df[, c(1, 3)], 
+      format = "html", 
+      escape = FALSE, 
+      col.names = c("File", "Raw URL")) %>%
+  kable_styling(full_width = FALSE, bootstrap_options = c("striped", "hover"))
 
+write_clip(html_tab)
