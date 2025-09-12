0a27c350f2d385e7d8ea84511a9a2ce80eba09ab
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-09-12 13:39:52 -0600

    added homework

--- PSYCH_413_fa2025/countdown.js
+++ PSYCH_413_fa2025/countdown.js
@@ -7,6 +7,7 @@ const countdownData = [
   { label: "Final Submission", date: "2025-12-19T08:30:00", timezone: "America/Edmonton", containerId: "Final2" },
 
   { label: "Homework 1", date: "2025-09-12T23:59:00", timezone: "America/Edmonton", containerId: "HW1" },
+  { label: "Homework 2", date: "2025-09-19T23:59:00", timezone: "America/Edmonton", containerId: "HW2" },
 
 ];
 
