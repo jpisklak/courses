794fd0def58cd24f9314a885b27316d69728db53
Jeffrey Pisklak <pisklak@ualberta.ca>
2025-10-28 09:25:48 -0600

    updated CPs

--- PSYCH_213_fa2025/countdown.js
+++ PSYCH_213_fa2025/countdown.js
@@ -9,6 +9,8 @@ const countdownData = [
   { label: "Homework 3", date: "2025-10-23T23:59:00", timezone: "America/Edmonton", containerId: "HW_3" },
   { label: "Homework 4", date: "2025-11-04T23:59:00", timezone: "America/Edmonton", containerId: "HW_4" },
   { label: "Course Project I", date: "2025-11-21T23:59:00", timezone: "America/Edmonton", containerId: "CP_1" },
+  { label: "Course Project II", date: "2025-11-28T23:59:00", timezone: "America/Edmonton", containerId: "CP_2" },
+
 ];
 
 // Function to create a timer in a specific container
