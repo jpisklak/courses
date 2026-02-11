01770448fd396a1973e93ea33ad1864b9256169e
Jeffrey Pisklak <pisklak@ualberta.ca>
2026-02-11 09:49:22 -0700

    updated dates

--- PSYCH_213_B4/countdown.js
+++ PSYCH_213_B4/countdown.js
@@ -6,7 +6,7 @@ const countdownData = [
   { label: "Achieve", date: "2026-04-10T23:59:00", timezone: "America/Edmonton", containerId: "Achieve_2" },
   { label: "Homework", date: "2026-01-30T23:59:00", timezone: "America/Edmonton", containerId: "HW1" },
   { label: "Homework", date: "2026-02-16T23:59:00", timezone: "America/Edmonton", containerId: "HW2" },
-  { label: "Homework", date: "2026-02-20T23:59:00", timezone: "America/Edmonton", containerId: "HW3" },
+  { label: "Homework", date: "2026-02-27T23:59:00", timezone: "America/Edmonton", containerId: "HW3" },
 
 ];
 
